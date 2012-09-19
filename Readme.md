53c7ddadd38c02fab947d7b6514e7caff566c6b5
Forbes Lindesay <fpfl2@cam.ac.uk>
2012-09-19 01:07:12 +0100

    Initial code

--- Readme.md
+++ Readme.md
@@ -2,3 +2,41 @@ editable
 ========
 
 Inline editing inspired by jEditable
+
+To provide the actual editing functionality you'll want one of the editor components.
+
+## Editor Components
+
+Completed:
+
+None yet...watch this space
+
+In Development
+
+ - Text
+ - Number
+ - Checkbox
+ - Date
+ - Currency
+
+## API
+
+This module provides an api for the other modules to use.  It is intended to provide neat wrappers for the shared functionality between all these other renderers.
+
+### editable.forEach(elements, fn)
+
+Equivalent to `Array.prototype.forEach` but works on sudo-arrays like NodeList and also treats non-arrays as single element arrays.  This lets you easilly make functions that can be called with one or many elements equivalently.
+
+### editable.click(elements, fn)
+
+Handle click for one or many elements, and call the function with the element and the MouseEvent.
+
+### editable.blur(elements, fn)
+
+Handle the blur event, same as click above.
+
+### editable.attribute(element, name, [value])
+
+If a value is provided, this sets the data attribute on the element.  If no value is provided then this searches the tree upwards for the provided data attribute.  This is useful for setting a record-id at the top level, but each individual field-name on the individual nodes.
+
+Attributes should be provided without the `data-` prefix.
---
+++ index.js
@@ -0,0 +1,68 @@
+
+/**
+ * Iterate over the elements and call the function as if forEach was used on an array.
+ *
+ * Will treat any non-arraylike object as an array containing a single item.
+ * 
+ * @param  {NodeList|Array|Element}                   elements
+ * @param  {Function(Element, Index, NodeList|Array)} fn
+ */
+function forEach(elements, fn) {
+  if (typeof elements.length === 'undefined') elements = [elements];
+  Array.prototype.forEach.call(elements, fn);
+}
+module.exports.forEach = forEach;
+
+/**
+ * Handle click for each element in the element list
+ * 
+ * @param  {NodeList|Array|Element}        elements
+ * @param  {Function(Element, MouseEvent)} fn
+ */
+function click(elements, fn) {
+  forEach(elements, function (element) {
+    element.addEventListener('click', function (eventArgs) {
+      return fn(element, eventArgs);
+    }, false);
+  });
+}
+module.exports.click = click;
+
+/**
+ * Handle the event fired when a(n) element(s) loos(es) focus
+ * 
+ * @param  {NodeList|Array|Element}        elements
+ * @param  {Function(Element, BlurEvent)}  fn
+ */
+function blur(elements, fn) {
+  forEach(elements, function (element) {
+    element.addEventListener('blur', function (eventArgs) {
+      return fn(element, eventArgs);
+    }, false);
+  });
+}
+module.exports = blur;
+
+/**
+ * Get a data attribute, and search up through the DOM tree to parent nodes if the attribute isn't present on the current node.
+ * 
+ * @param  {Element} element
+ * @param  {String}  name    will have 'data-' prepended before passing to getAttribute
+ * @param  {Any}     [value] If present the method will act as setAttribute
+ */
+function attribute(element, name, value) {
+  if (!/^data\-/.test(name)) {
+    name = 'data-' + name;
+  }
+  if (arguments.length === 3) {
+    element.setAttribute(name, value);
+  } else {
+    var res = element.getAttribute(name);
+    while ((res === null || res === '') && element.parentNode !== null) {
+      element = element.parentNode;
+      res = element.getAttribute(name);
+    }
+    return res === '' ? null : res;
+  }
+}
+module.exports = attribute;
